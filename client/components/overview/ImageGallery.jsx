abb145019983f8d732e9001cbf0399152fd8e7fa
al <aherna4@gmail.com>
2021-06-15 14:24:16 -0500

    uploading data post crash

--- client/components/overview/ImageGallery.jsx
+++ client/components/overview/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Grid} from '@material-ui/core';
 import ArrowForward from '@material-ui/icons/ArrowForwardIos';
 import ArrowBack from '@material-ui/icons/ArrowBackIos';
@@ -6,20 +6,23 @@ import ArrowBack from '@material-ui/icons/ArrowBackIos';
 const ImageGallery = ({currentStyle}) => {
 
   const [currentImage, setCurrentImage] = useState(0)
+  const [ArrayLength, setArrayLength] = useState(0)
 
-  if (currentStyle) {
-    const photosArrayLength = currentStyle.photos.length
-    console.log(photosArrayLength)
-  }
+  useEffect(() => {
+      setArrayLength(currentStyle.photos.length)
+  }, [currentStyle])
+
+
 
   const handleNextImageClick = () => {
     console.log(photosArrayLength)
-    if (photosArrayLength !== undefined) {
-      setCurrentImage(currentImage === photosArrayLength -1 ? 0 : current + 1)
-    } else {
-      console.log('oops')
-    }
+    // if (photosArrayLength !== undefined) {
+    //   setCurrentImage(currentImage === photosArrayLength -1 ? 0 : current + 1)
+    // } else {
+    //   console.log('oops')
+    // }
   }
+
   const handlepreviousImageClick = () => {
 
   }
