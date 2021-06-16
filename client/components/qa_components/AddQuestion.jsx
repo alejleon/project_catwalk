f24343f664e9bb8fb17329fe5344ed343c74b8d5
alejleon <aherna4@gmail.com>
2021-06-16 18:08:10 -0500

    nothing here

--- client/components/qa_components/AddQuestion.jsx
+++ client/components/qa_components/AddQuestion.jsx
@@ -1,16 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from'@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 import AddQuestionForm from './AddQuestionForm.jsx'
 
 const AddQuestion = (props) => {
 
   return (
+
     <React.Fragment>
       <Dialog open={props.open} onClose={props.handleQClose}>
         <DialogTitle id="question-dialog-title">Ask Your Question</DialogTitle>
